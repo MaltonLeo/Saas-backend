f9a4deaf8335b6e34cedfb6c2b35943b9521f6c5
MaltonLeo <sherzodkhudaynazarov1@gmail.com>
2025-05-05 19:35:16 +0900

    weedwe

--- config/settings.py
+++ config/settings.py
@@ -125,10 +125,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-CORS_ALLOW_ALL_ORIGINS = True 
+CORS_ALLOWED_ORIGINS = [
+    "https://magnificent-fox-b96890.netlify.app",  # Netlify frontend URL
+]
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
+CORS_ALLOW_CREDENTIALS = True
 
 STATIC_URL = 'static/'
 
